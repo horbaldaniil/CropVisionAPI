636abfe73a3956d78d49f8fe36080e2bf018567d
Horbal Daniil <daniil.horbal@lnu.edu.ua>
2025-06-14 18:48:08 +0300

    Render settings update

--- main.py
+++ main.py
@@ -6,12 +6,3 @@ app = FastAPI()
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(model.router)
 
-
-if __name__ == "__main__":
-    uvicorn.run(
-        "main:app",
-        host="127.0.0.1",
-        port=8000,
-        reload=True
-    )
-
